520a583b1b8a8a82dce939575d87ed2b2d720615
Ludvig Ohlsson <ludohl@student.chalmers.se>
2020-10-20 23:42:22 +0200

    Fixed assignment 3(b)
    Now correctly uses an adjacency matrix and adds '(1 - λ)/n_i' when it should.

--- pagerank.jl
+++ pagerank.jl
@@ -9,28 +9,35 @@ P = [λ/n            (1-λ)/3+λ/n   (1-λ)/3+λ/n   (1-λ)/3+λ/n
     ]
 
 q = [1/4 1/4 1/4 1/4]
-print(q)
 
 sol = q*P^1000000000000000
 
-P2 = [0 1/3 1/3 1/3
-     0 0 1/2 1/2
-     1 0 0 0
-     1/2 0 1/2 0
-    ]
-for row in eachrow(P)
-        println(row)
-end
-
-
 #Assignment 3(b)
-adjtolam(P2,λ,n)
+function adjtolam!(A, λ)
+    n = length(eachrow(A))
 
-function adjtolam(P, λ, n)
-    for row in eachrow(P)
-        for i in 1:size(row)
-            row[i] += λ/n
+    for row in eachrow(A)
+        n_i = 0
+        for i in eachindex(row)
+            if row[i] != 0
+                n_i += 1
+            end
+        end
+
+        for i in eachindex(row)
+            row[i] = (row[i] != 0 ? (1 - λ)/n_i : 0) + λ/n
         end
     end
-    return P
+
+    return A
 end
+
+A = [0.0 1.0 1.0 1.0
+     0.0 0.0 1.0 1.0
+     1.0 0.0 0.0 0.0
+     1.0 0.0 1.0 0.0
+    ]
+
+adjtolam!(A, λ)
+
+A == P # should be true after running above function
